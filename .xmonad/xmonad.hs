b912f002c7aeee7890796e0bc4bd11581838d7d3
Justin Mazzi <jmazzi@gmail.com>
2012-10-08 11:37:48 -0400

    Use pydmenu

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -141,7 +141,7 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
   -- Launch dmenu via yeganesh.
   -- Use this to launch programs without a key binding.
   , ((modMask, xK_p),
-     spawn "exe=`dmenu_path | yeganesh` && eval \"exec $exe\"")
+     spawn "exe=`dmenu_path | ~/.xmonad/bin/pydmenu.py` && eval \"exec $exe\"")
 
   -- Take a screenshot in select mode.
   -- After pressing this key binding, click a window, or draw a rectangle with
